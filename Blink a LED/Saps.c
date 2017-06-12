da6e5fc2980cc139135601722c73f966b1e0d9e9
SamYarbrough96 <samyarbrough96@gmail.com>
2017-06-12 11:25:17 -0500

    Add files via upload

---
+++ Blink a LED/Saps.c
@@ -0,0 +1,19 @@
+/*
+  * Sam Yarbrough
+  * Progrsm #2
+  * BLINK DAT LED
+  * 06/12/17 
+*/
+#include "simpletools.h"                   
+
+int main()                                    
+{
+ 
+  while(9999)
+  {
+    high(0);
+    high(15);
+   ;
+    
+  }  
+}
