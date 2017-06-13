8ebbdca62628b73841eff2f2946e20782692ba75
SamYarbrough96 <samyarbrough96@gmail.com>
2017-06-13 09:57:21 -0500

    Add files via upload

---
+++ Microcontroller-Camo/Counting/SKRT SKRT.c
@@ -0,0 +1,18 @@
+/*
+ * Sam Yarbrough
+ * Counting Looop
+ * 06/13/17
+*/
+#include "simpletools.h"
+
+int main()                                    
+{
+ int x = 1;
+ 
+ while (x < 11)
+ {
+   print("%d\n", x);
+   x = x + 1;
+ }
+ print("Done"); 
+}
---
+++ Microcontroller-Camo/Counting/whatever.c
@@ -0,0 +1,14 @@
+
+#include "simpletools.h"
+                      
+int main()                                    
+{
+  int x = 33;
+ 
+  while (x < 128)
+  {
+   print("%c\n", x);
+   x = x + 1;
+  }
+ print("Done"); 
+}
